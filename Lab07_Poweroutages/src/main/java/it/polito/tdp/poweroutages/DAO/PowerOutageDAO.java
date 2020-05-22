3b93f4aea983b6f0a34d6b3ebe73abe028601813
pietrodorto <s249615@studenti.polito.it>
2020-05-22 19:36:29 +0200

    getpoweroutages DAO

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/DAO/PowerOutageDAO.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/DAO/PowerOutageDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.poweroutages.model.Nerc;
+import it.polito.tdp.poweroutages.model.PowerOutages;
 
 public class PowerOutageDAO {
 	
@@ -33,6 +34,30 @@ public class PowerOutageDAO {
 		}
 
 		return nercList;
+	}
+	
+	public List<PowerOutages> getPowerOutages() {
+
+		String sql = "SELECT id, event_type_id, area_id, nerc_id, customers_affected, date_event_began, date_event_finished FROM poweroutages";
+		List<PowerOutages> poweroutagesList = new ArrayList<>();
+
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				PowerOutages po = new PowerOutages(res.getInt("id"),res.getInt("event_type_id"),res.getInt("area_id"),res.getInt("nerc_id"),res.getInt("customers_affected"), res.getDate("date_event_began"), res.getDate("date_event_finished"));
+				poweroutagesList.add(po);
+			}
+
+			conn.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return poweroutagesList;
 	}
 	
 
---
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/model/PowerOutages.java
@@ -0,0 +1,93 @@
+package it.polito.tdp.poweroutages.model;
+
+import java.util.Date;
+
+public class PowerOutages {
+	int id;
+	int event_type_id;
+	int area_id;
+	int nerc_id;
+	int customers_affected;
+	Date data_event_began;
+	Date data_event_finished;
+	
+	
+	
+	public PowerOutages(int id, int event_type_id, int area_id, int nerc_id, int customers_affected,
+			Date data_event_began, Date data_event_finished) {
+		super();
+		this.id = id;
+		this.event_type_id = event_type_id;
+		this.area_id = area_id;
+		this.nerc_id = nerc_id;
+		this.customers_affected = customers_affected;
+		this.data_event_began = data_event_began;
+		this.data_event_finished = data_event_finished;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getEvent_type_id() {
+		return event_type_id;
+	}
+	public void setEvent_type_id(int event_type_id) {
+		this.event_type_id = event_type_id;
+	}
+	public int getArea_id() {
+		return area_id;
+	}
+	public void setArea_id(int area_id) {
+		this.area_id = area_id;
+	}
+	public int getNerc_id() {
+		return nerc_id;
+	}
+	public void setNerc_id(int nerc_id) {
+		this.nerc_id = nerc_id;
+	}
+	public int getCustomers_affected() {
+		return customers_affected;
+	}
+	public void setCustomers_affected(int customers_affected) {
+		this.customers_affected = customers_affected;
+	}
+	public Date getData_event_began() {
+		return data_event_began;
+	}
+	public void setData_event_began(Date data_event_began) {
+		this.data_event_began = data_event_began;
+	}
+	public Date getData_event_finished() {
+		return data_event_finished;
+	}
+	public void setData_event_finished(Date data_event_finished) {
+		this.data_event_finished = data_event_finished;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PowerOutages other = (PowerOutages) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+}
